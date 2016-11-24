3999eedbce83b7f1ac4989c517e48c86974e3998
TheOminousMango <epicishness@live.com>
2016-11-24 13:38:16 -0500

    added permissions for qr

--- collections/Qr_Code.js
+++ collections/Qr_Code.js
@@ -1,9 +1,28 @@
 QrCode = new Meteor.Collection('qr_code');
 
+QrCode.deny({
+  insert: function() {
+    return true;
+  },
+  remove: function() {
+    return true;
+  },
+  update: function() {
+    return true;
+  }
+});
+
 QrCodeSchema = new SimpleSchema({
   _code: {
     type: String,
     label: "Content",
+    autoValue: function() {
+      var content = "";
+      for(var i = 0; i < 32; i++) {
+            content += Math.round(Math.random());
+      }
+      return content;
+    }
   },
 
   createdAt: {
